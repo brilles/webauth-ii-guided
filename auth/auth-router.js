9c6e514fe72c085b7e20dc9f54629df8b1487bee
Brian Illes <brianrilles@gmail.com>
2019-04-02 11:16:04 -0700

    complete

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -25,8 +25,11 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        // req.session is added by express-session
+        req.session.user = user;
+
         res.status(200).json({
-          message: `Welcome ${user.username}!`,
+          message: `Welcome ${user.username}!`
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -35,6 +38,20 @@ router.post('/login', (req, res) => {
     .catch(error => {
       res.status(500).json(error);
     });
+});
+
+router.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.status(500).json({ message: 'cant leave' });
+      } else {
+        res.status(200).json({ message: 'bye' });
+      }
+    });
+  } else {
+    res.status(200).json({ message: 'bye' });
+  }
 });
 
 module.exports = router;
--- auth/session-config.js
+++ auth/session-config.js
@@ -1,16 +1,8 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
 const session = require('express-session');
 const KnexSessionStore = require('connect-session-knex')(session);
 // returns a function that returns a constructor function when session passed
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
-const configuredKnex = require('../database/dbConfig.js');
-const server = express();
-
-const sessionConfig = {
+module.exports = {
   name: 'monster',
   secret: 'keep it secret, keep it safe!',
   cookie: {
@@ -28,17 +20,3 @@ const sessionConfig = {
     clearInterval: 1000 * 60 * 30 // delete expired sessions
   })
 };
-
-server.use(helmet());
-server.use(express.json());
-server.use(cors());
-server.use(session(sessionConfig));
-
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
-
-server.get('/', (req, res) => {
-  res.send("It's alive!");
-});
-
-module.exports = server;
